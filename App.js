c31393c490ed52c6577e997fcc49704dd64be70d
chernandez7 <chris00hernandez@gmail.com>
2019-10-01 22:21:41 -0500

    update folder structure

--- App.js
+++ App.js
@@ -1,36 +1,7 @@
 import React from "react";
-import { Text, View, TouchableWithoutFeedback } from "react-native";
 
-import Button from "./src/Button";
-import Style from "./App.style";
+import App from "./src/App";
 
-class App extends React.Component {
-  state = {
-    count: 0
-  };
+const AppContainer = () => <App />;
 
-  increment = () => {
-    const { count } = this.state;
-    this.setState({ count: count + 1 });
-  };
-
-  decrement = () => {
-    const { count } = this.state;
-    this.setState({ count: count - 1 });
-  };
-
-  render() {
-    const { count } = this.state;
-    return (
-      <View style={Style.container}>
-        <Text style={Style.count}>{count}</Text>
-        <View style={Style.buttonsContainer}>
-          <Button text="-" onPress={() => this.decrement()} />
-          <Button text="+" onPress={() => this.increment()} />
-        </View>
-      </View>
-    );
-  }
-}
-
-export default App;
+export default AppContainer;
---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Text, View, TouchableWithoutFeedback } from "react-native";
+
+import Button from "./Button";
+import Style from "./App.style";
+
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    const { count } = this.state;
+    this.setState({ count: count + 1 });
+  };
+
+  decrement = () => {
+    const { count } = this.state;
+    this.setState({ count: count - 1 });
+  };
+
+  render() {
+    const { count } = this.state;
+    return (
+      <View style={Style.container}>
+        <Text style={Style.count}>{count}</Text>
+        <View style={Style.buttonsContainer}>
+          <Button text="-" onPress={() => this.decrement()} />
+          <Button text="+" onPress={() => this.increment()} />
+        </View>
+      </View>
+    );
+  }
+}
+
+export default App;
